ad385b15a4ec64a436d02036a7490cc59f8ec925
Ilian Igoudjil <ilianigoudjil@gmail.com>
2025-01-31 16:00:31 +0100

    Création du déclencheur t_store_offer_updates

--- src/level_5.sql
+++ src/level_5.sql
@@ -1,2 +1,18 @@
 -- EXERCICE 1 ##################################
 -- Trriger store_offer_updates
+CREATE OR REPLACE FUNCTION store_offer_updates()
+RETURNS trigger AS
+$$
+BEGIN
+    IF OLD.price IS DISTINCT FROM NEW.price THEN
+        INSERT INTO offers_history (offer_code, old_price, new_price)
+        VALUES (OLD.code, OLD.price, NEW.price);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER t_store_offer_updates
+AFTER UPDATE OF price ON offer
+FOR EACH ROW
+EXECUTE FUNCTION store_offer_updates();
